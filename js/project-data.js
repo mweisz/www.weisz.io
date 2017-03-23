46f373e2f0fa32ad430b890ca665dd471616b44e
Michael Weisz <mail@michael-weisz.de>
2017-03-23 02:10:02 +0000

    Add project descriptions as modals.

---
+++ js/project-data.js
@@ -0,0 +1,53 @@
+var projectData = {
+    'become-the-driver': {
+        'title' : 'Become the Driver',
+        'subtitle' : 'Become the Driver',
+        'creators' : 'Jonas Keutel, Fabian Wiebe, Elizabeth Williams, Michael Weisz',
+        'description' : '<p>Created prototype for 3D application aiming to make motorsport events more interactive and immersive by adding features such as Social and Gamification.</p> <p>The systems (created at the SAP Innojam 2015 in Palo Alto) enables you to compete against your friends and receive points based on a selected driver\'s performance in an actual motorsport event that is broadcasted on TV.</p>',
+        'imgPath' : 'img/portfolio/become-the-driver.jpg',
+        'footerNote' : 'Created at SAP Innojam 2016.'
+    },
+    'remote-gardening': {
+        'title' : 'Remote Farming',
+        'subtitle' : 'Remote Farming',
+        'creators' : 'Lars Butzmann, Leo Kotschenreuther, Stefan Klauck, Stephan Schultz, Michael Weisz',
+        'description' : '<p>You probably know about FarmVille, a game where you can build a farm in your browser. The problem is that you only have a virtual farm so you will never eat your harvested fruits. Remote farming provides the possibility to plant, water, fertilize, and harvest your own plants in real life taking into account concepts like social media and gamification. This app enables you to monitor the growth of your plants by checking live sensor data and even lets you watch the plants grow using a Webcam – literally.</p>',
+        'imgPath' : 'img/portfolio/remote-gardening.jpg',
+        'url' : 'https://github.com/mweisz/InnoJam-Farmville'
+
+    },
+    'future-ide': {
+        'title' : 'Collaborative IDE',
+        'subtitle' : 'Receive help when you need it',
+        'creators' : 'Pascal Crenzin, Michael Janke, Michael Weisz',
+        'description' : '<p>Context sensitive integrated development environment (IDE) that automatically recognises when you get stuck and lets you collaborate with your friends / colleagues in real-time by using screen sharing. </p> <p>Potential helpers are selected based on their skillset and the current problem context.</p>' ,
+        'imgPath' : 'img/portfolio/future-ide.jpg'
+
+    },
+    'virtual-coach': {
+        'title' : 'Virtual Fitness Coach',
+        'subtitle' : 'Virtual Fitness Coach',
+        'creators' : 'Jonas Keutel, Michael Weisz',
+        'description' : '<p>Created prototype for an interactive system that allows people to do exercises (e.g. squats, weight-lifting, etc.) while receiving ongoing feedback and a post-analysis regarding the correctness of their execution. The main goal is to provide a more effective training and to prevent injuries.</p> <p>The system follows the metaphor of having your own personal trainer and was implemented using Microsoft-Kinect cameras to perform real-time motion capturing.</p><p>We had the opportunity to showcase the project at the Mobile World Congress 2016 in Barcelona.</p>' ,
+        'imgPath' : 'img/portfolio/virtual-coach.jpg'
+
+    },
+    'dynamic-tour-planning': {
+        'title' : 'Dynamic Tour Planning',
+        'subtitle' : 'Dynamic Tour Planning',
+        'creators' : 'Marvin Bornstein, Hagen Echzell, Leo Kotschenreuther, René Springer, Fabian Wiebe, Michael Weisz',
+        'description' : '<p>Research project at the chair of Prof. Hasso Plattner in collaboration with Colgate Palmolive (CP) exploring new approaches to multi-constraint vehicle routing problems (VRP) leveraging large amounts of Point-of-Sales (POS) data in real-time.</p> <p>Designed and implemented algorithms/heuristics for variations of the VRP and created a working prototype for the sales representatives of CP. Regular exchange in a global team (USA, France, Germany) and final presentation to members of the board of Colgate-Palmolive.</p>' ,
+        'imgPath' : 'img/portfolio/algorithms-for-dynamic-tour-planning.jpg',
+        'url' : 'https://hpi.de/plattner/projects/project-archive/dynamic-tour-planning.html'
+
+    },
+    'quote-of-the-day': {
+        'title' : 'OSX: Quote of the Day for Lockscreen',
+        'subtitle' : 'Quote of the Day for Mac Lockscreen',
+        'creators' : 'Michael Weisz',
+        'description' : '<p>Provides a "Quote of the Day" feature for the logon/lock screen of OS X. </p> <p>Reads a random line from a provided text file or takes a quote from a publicly available source like wikiquote.org and sets it as lock screen message.</p>' ,
+        'imgPath' : 'img/portfolio/quote-of-the-day.jpg',
+        'url' : 'https://github.com/mweisz/mac-lockscreen-quote-of-the-day'
+
+    }
+}
---
+++ js/project-descriptions.js
@@ -0,0 +1,57 @@
+function onProjectClick(projectId) {
+    
+    var project = projectData[projectId];
+
+    // Title
+    var title = project['title'];
+
+    // Subtitle
+    var subtitle = project['subtitle'];
+
+    // Creators
+    var creators = project['creators'];
+
+    // Description
+    var description = project['description'];
+
+    // Description Image
+    var imgPath = project['imgPath'];
+
+    var projectURL = project['url'];
+
+
+    var websiteHTML = projectURL ? '<a href="'+projectURL+'">Project Website</a>' : '';
+    var html = '<div class="media">' +
+                '<h4 class="media-heading">' + title + '</h4>' +
+                '<h5 class="media-subheading">By ' + creators + '</h5>' +
+              '<div class="media-body">' +
+                 '<img class="pull-left description-image" src="' + imgPath +'" alt="...">' +
+                description +
+              '</div>' +
+              '<div class="media-footer">' +
+                    '<span class="media-footer-left"></span><span class="media-footer-right">'+ websiteHTML+' </span>' +
+              '</div>' +
+            '</div>';    
+
+            // var html = '<div class="media">' +
+            //     '<h4 class="media-heading">' + title + '</h4>' +
+            //     '<h5 class="media-subheading">By ' + creators + '</h5>' +
+
+            //   '<div class="media-left">' +
+            //      '<img class="media-object" src="' + imgPath +'" alt="...">' +
+            //   '</div>' +
+            //   '<div class="media-body">' +
+            //     description +
+            //   '</div>' +
+            //   '<div class="media-footer">' +
+            //         '<span class="media-footer-left"></span><span class="media-footer-right">'+ websiteHTML+' </span>' +
+            //   '</div>' +
+            // '</div>';
+
+    var dialog = bootbox.dialog({
+        message: html,
+        onEscape: true,
+        backdrop: true,
+        size: 'large'
+    });
+}
